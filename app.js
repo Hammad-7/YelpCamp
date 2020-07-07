60482248d3e2137bf3847e42fbf92a7b83891371
Hammad-7 <hammadbasithb@gmail.com>
2020-07-07 09:15:17 +0530

    Adding login logic

--- app.js
+++ app.js
@@ -136,6 +136,16 @@ app.post("/register",function(req,res){
     })
 })
 
+app.get("/login",function(req,res){
+    res.render("login");
+})
+
+app.post("/login",passport.authenticate("local",{
+    successRedirect:"/campgrounds",
+    failureRedirect:"/login"
+}),function(req,res){
+})
+
 
 
 app.listen(3000,function(){
