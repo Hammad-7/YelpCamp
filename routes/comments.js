b18c49771ec3dc711fa90511e1f24244e2f850c3
Hammad-7 <hammadbasithb@gmail.com>
2020-07-08 10:42:36 +0530

    Comment Authorization

--- routes/comments.js
+++ routes/comments.js
@@ -40,6 +40,43 @@ router.post("/",isLoggedIn,function(req,res){
     })
 })
 
+//Comment Edit Route
+router.get("/:comment_id/edit",checkCommentOwnership,function(req,res){
+    Comment.findById(req.params.comment_id,function(err,foundComment){
+        if(err){
+            console.log(err);
+            res.redirect("back")
+        } else{
+            res.render("comments/edit",{
+                campground_id:req.params.id,
+                comment:foundComment})
+        }
+    })
+})
+
+//Comment Update
+router.put("/:comment_id",checkCommentOwnership,function(req,res){
+    Comment.findByIdAndUpdate(req.params.comment_id,req.body.comment,function(err,updatedComment){
+        if(err){
+            res.redirect("back")
+        } else {
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    })
+})
+
+//Comment Destroy
+router.delete("/:comment_id",checkCommentOwnership,function(req,res){
+    Comment.findByIdAndDelete(req.params.comment_id,function(err){
+        if(err){
+            console.log(err);
+            res.redirect("back");
+        } else{
+            res.redirect("/campgrounds/"+req.params.id);
+        }
+    })
+})
+
 function isLoggedIn(req,res,next){
     if(req.isAuthenticated()){
         return next();
@@ -47,5 +84,22 @@ function isLoggedIn(req,res,next){
     res.redirect("/login");
 }
 
+function checkCommentOwnership(req,res,next){
+    if(req.isAuthenticated()){
+        Comment.findById(req.params.comment_id,function(err,foundComment){
+            if(err){
+                res.redirect("back");
+            } else{
+                if(foundComment.author.id.equals(req.user._id)){
+                    next();
+                } else{
+                    res.redirect("back");
+                }
+            }
+        })
+    } else{
+        res.redirect("back");
+    }
+}
 
 module.exports = router;
